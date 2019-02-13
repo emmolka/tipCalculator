4c690bdcdd54528e115923077bc0eb59ae97c8bc
Master <git@github.com:emmolka/NavBar.git>
2019-02-14 00:28:01 +0100

    buttonsAdded

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 let percentage;
-document.querySelector('.selected').addEventListener('click', function(){
-    let number =document.querySelector('.selected').value;
-    
-    
-    
-    if(number==='1'){
-        percentage=0.2;
-    }else if(number==='2'){
-        percentage=0.15;
-    }else if(number==='3'){
-        percentage=0.1;
-    }else if(number==='4'){
-        percentage=0.05;
-    }
-    
+document.querySelector('.first').addEventListener('click', function(){
+    percentage=0.05;
     
 });    
+document.querySelector('.second').addEventListener('click', function(){
+    percentage= 0.1;
+    
+});
+document.querySelector('.third').addEventListener('click', function(){
+    percentage= 0.15;
+    
+});
+document.querySelector('.fourth').addEventListener('click', function(){
+    percentage= 0.2;
+});
+
 document.querySelector('.button').addEventListener('click', function(){
     let bill = document.querySelector('.bill-amount').value;
     let people= document.querySelector('.people-sharing').value;
